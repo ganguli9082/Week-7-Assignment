431e8e384cee07e37a0f8fa4f24d0df9db32992b
Alex Ganguli <50713465+ganguli9082@users.noreply.github.com>
2019-06-30 22:33:28 -0700

    update
    Queue is working. Runner program had queue class and exceptions working

--- src/Queue.java
+++ src/Queue.java
@@ -8,53 +8,72 @@ public class Queue {
 	
 	public Queue()
 	{
+		//System.out.println("calling  Queue");
 		queue = new char[10];
-		
+		size = 0;
+		front = 0;
+		rear = 0;
 	}
+	
 	
 	public void enQueue(char CHAR)
 	{
+		//System.out.println("calling  enQueue");
+
 		queue[rear] = CHAR;
-		rear = (rear++)%10;
-		size = size++;
-		
+		rear = (rear+1)%10;
+		size++;
+		//System.out.println(queue);
+		//System.out.println(rear);
+		//System.out.println(size);
 	}
 	
 	
 	public char deQueue()
 	{
+		//System.out.println("debug deQueue: " + size);
+		//System.out.println(queue);
+		//System.out.println(size);
+		//System.out.println(front);
 	    char CHAR = queue[front];
-		front = (front++)%10;
-		size = size--;
+		front = (front+1)%10;
+		size = size-1;
+		
+		//System.out.println(queue[front]);
+		//System.out.println(size);
+		//System.out.println(front);
 		return CHAR;
+		
 	}
 	
 	
 	
 	public String toString()
 	{
-		String report = "Characters: ";
-		//for (int index = 0; index< size; index++)
+		//System.out.println("calling  toString");
+		//int index = front;
+		//String report = "Characters: ";
+		//for (;index< size; index++)
 		//	report += queue[index] + "/n";
-		return report;
 		
-		//char result = ' ';
-		//for(int i = 0; i<size;i++)
-		//{
-		//	result += queue[(front + i)%10];
-		//	System.out.println("Characters" + result);
-		//}
-		//System.out.println(" ");
+		String result = "";
+		for(int i = 0; i<size;i++)
+		{
+			result += queue[(front + i)%10];
+			
+		}
 		
+		return result;
 		//for (int n : queue)
 		//{
 		//	System.out.println(n + " ");
 		//}
 	}
-	public int size(int SIZE)
+	public int size()
 	{
-		size = SIZE;
-		return SIZE;
+		//System.out.println("calling  size");
+		
+		return size;
 	}
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,11 @@
 
 public class Runner {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ExceptionQueue {
+		ExceptionQueue Limit10 = new ExceptionQueue("You entered more than 10 characters");
+		ExceptionQueue EmptyQueue = new ExceptionQueue("Queue is empty. Cannot Dequeue");
+		int i = 0;
+		String test;
 		Queue q = new Queue();
 		q.enQueue('A');
 		q.enQueue('d');
@@ -12,13 +15,36 @@ public class Runner {
 		q.enQueue('d');
 		q.enQueue('d');
 		q.enQueue('d');
+		q.size();
+		if (q.size()>10)
+			throw Limit10;
 		
-		q.toString();
+		//System.out.println("before init test.");
+		//test = q.toString();
+		//toString();
 		
 		
 		
 		q.deQueue();
-	
+		q.deQueue();
+		q.deQueue();
+		q.deQueue();
+		q.deQueue();
+		q.deQueue();
+		q.deQueue();
 		
+		q.enQueue('z');
+		q.enQueue('z');
+		q.enQueue('z');
+		q.enQueue('z');
+		q.enQueue('z');
+	
+		//q.toString();
+		System.out.print(q);
+		
+		
+		q.size();
+		if (q.size()<=0)
+			throw EmptyQueue;
 	}
 }
